08781de1323ef35a6b510f51ae1e694fb3857d25
Saleha Fathima <143305167+salehafathimazaman@users.noreply.github.com>
2026-04-16 23:50:31 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+
+resource "azurerm_resource_group" "sfz_rg" {
+  name     = var.resource_group_name
+  location = "westus2"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+
+
